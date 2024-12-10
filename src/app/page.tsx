39a542087f13578778b72c4cc560903af8cb56c0
CarolinaBarb <carolinabarbosab09@gmail.com>
2024-12-10 14:08:00 -0300

    iniciando componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/header";
+import HorizontalCard from "@/components/horizontalcard";
 import Square from "@/components/square";
 
 export default function Home() {
   return (
     <div className="mx-16 -z-10 flex items-center justify-center flex-col mt-24 gap-16">
       <Square>
-        Oi leo
+        <HorizontalCard color="bg-primaria03" text="estou fazendo o site"  description="muito legal" img="lallaala"/>
       </Square>
     </div>
   );
