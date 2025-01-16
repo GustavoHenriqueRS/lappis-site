224b3631176aedb8acd44af91c43d8cc2e160953
YuriBre <brenoyuri5000@gmail.com>
2025-01-16 16:06:47 -0300

    iniciando responsividade home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,24 +22,24 @@ export default async function Home() {
 
   return (
     <div className="flex items-center justify-center flex-col gap-16">
-      <Square className="p-8 flex flex-col items-center gap-8">
+      <Square className="p-3 sm:p-4 flex flex-col items-center gap-8">
       <div className="flex justify-center w-full h-[400px] sm:h-[500px] md:h-[700px] relative">
-  <video
-    className="rounded-[12px] w-full h-full object-cover"
-    autoPlay
-    loop
-    playsInline
-    muted
-  >
-    <source
-      src="https://videos.pexels.com/video-files/3251841/3251841-uhd_2560_1440_25fps.mp4"
-      type="video/mp4"
-    />
-  </video>
-  <button className="absolute bottom-4 sm:bottom-8 bg-primaria04 text-white text-base sm:text-lg md:text-2xl px-8 sm:px-12 md:px-16 py-3 sm:py-4 md:py-5 rounded-xl hover:bg-secundaria700Magenta">
-    Conhecer projetos
-  </button>
-</div>
+        <video
+          className="rounded-[12px] w-full h-full object-cover"
+          autoPlay
+          loop
+          playsInline
+          muted
+        >
+          <source
+            src="https://videos.pexels.com/video-files/3251841/3251841-uhd_2560_1440_25fps.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <button className="absolute bottom-4 sm:bottom-8 bg-primaria04 text-white text-base sm:text-lg md:text-2xl px-8 sm:px-12 md:px-16 py-3 sm:py-4 md:py-5 rounded-xl hover:bg-secundaria700Magenta">
+          Conhecer projetos
+        </button>
+      </div>
 
       </Square>
 
--- src/components/squareTitle.tsx
+++ src/components/squareTitle.tsx
@@ -8,7 +8,7 @@ export default function SquareTitle({ title, color, ref }: SquareTitleProps) {
   return (
     <div
       ref={ref}
-      className={`px-4 sm:px-6 py-3 sm:py-4 bg-${color} rounded-xl flex items-center font-orbitron text-white absolute left-4 sm:left-8 mt-4 sm:mt-8`}
+      className={`px-4 sm:px-6 py-3 sm:py-4 bg-${color} rounded-xl flex items-center font-orbitron text-white absolute left-4 sm:left-8 mt-6 sm:mt-8`}
     >
       <h1 className="text-2xl sm:text-3xl md:text-4xl">{title}</h1>
     </div>
